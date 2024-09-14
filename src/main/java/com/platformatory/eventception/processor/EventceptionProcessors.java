d5aed362d3209e85628b76bf700417f3cb63721b
Avinash Upadhyaya <avinash@platformatory.com>
2024-09-14 14:32:05 +0530

    fix logging

--- src/main/java/com/platformatory/eventception/processor/EventceptionProcessors.java
+++ src/main/java/com/platformatory/eventception/processor/EventceptionProcessors.java
@@ -1,10 +1,30 @@
 package com.platformatory.eventception.processor;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
 // import com.dashjoin.jsonata.JSONata;
 // import com.platformatory.eventception.processor.ServiceConfig.TopologyConfig.OutputConfig;
 import com.platformatory.eventception.processor.ServiceConfig.TopologyConfig.ProcessorConfig;
 
+import dev.cel.common.CelAbstractSyntaxTree;
+import dev.cel.common.CelValidationException;
+import dev.cel.common.CelValidationResult;
+import dev.cel.common.ast.Expression;
+import dev.cel.common.types.SimpleType;
+import dev.cel.compiler.CelCompiler;
+import dev.cel.compiler.CelCompilerFactory;
+import dev.cel.expr.ExprValue;
+import dev.cel.expr.Value;
+import dev.cel.parser.CelParser;
+import dev.cel.parser.CelParserFactory;
+import dev.cel.runtime.Activation;
+import dev.cel.runtime.CelEvaluationException;
+import dev.cel.runtime.CelRuntime;
+import dev.cel.runtime.CelRuntimeFactory;
+
+import org.apache.kafka.streams.processor.To;
 // import dev.cel.expr.CEL;
 // import dev.cel.expr.CELBuilder;
 // import dev.cel.expr.ExprValue;
@@ -19,6 +39,79 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EventceptionProcessors {
+
+    public static class CelFilter implements Processor<String, String, String, String> {
+
+    private ProcessorContext<String, String> context;
+    private ProcessorConfig config;
+
+    public CelFilter(ProcessorConfig config) {
+        this.config = config;
+    }
+
+    public static Map<String, Object> parseJson(String jsonString) throws JsonMappingException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(jsonString, Map.class);
+    }
+
+    @Override
+    public void init(ProcessorContext<String, String> context) {
+        this.context = context;
+    }
+
+    @Override
+    public void process(Record<String, String> record) {
+        try {
+            String key = record.key();
+            String value = record.value();
+            Map<String, Object> jsonData = parseJson(value);
+            // Prepare the data to evaluate
+            // for (Map.Entry<String, Object> entry : jsonData.entrySet()) {
+            //     String key = entry.getKey();
+            //     Object value = entry.getValue();
+
+            //     // Create a CEL variable
+            //     ExprValue celVar = ExprValue.of(value); // auto infers CEL type
+
+            //     // Add the variable to CEL environment
+            //     celRuntime.setVariable(key, celVar);
+            // }
+            // Activation activation = Activation.of(null, ImmutableMap.copyOf(jsonData));
+            // Map<String, Value> variables = new HashMap<>();
+            // variables.put("key", Value.newBuilder().setStringValue(key).build());
+            // variables.put("value", Value.newBuilder().setStringValue(value).build());
+            CelCompiler celCompiler = CelCompilerFactory.standardCelCompilerBuilder().setResultType(SimpleType.BOOL).build();
+            // Initialize the CEL runtime and parse the CEL expression
+            CelValidationResult parseResult = celCompiler.parse(this.config.getCelExpression());
+            CelValidationResult checkResult = celCompiler.check(parseResult.getAst());
+            CelAbstractSyntaxTree ast = checkResult.getAst();
+            CelRuntime celRuntime = CelRuntimeFactory.standardCelRuntimeBuilder().build();
+            // Evaluate the CEL expression
+            CelRuntime.Program program = celRuntime.createProgram(ast);
+            // boolean result = celRuntime.eval(this.celExpression, variables)
+            //                   .getBoolValue();
+            boolean result = (boolean) program.eval(ImmutableMap.copyOf(jsonData));
+
+            // Forward the record if the CEL expression evaluates to true
+            if (result) {
+                context.forward(new Record<>(key, value, record.timestamp()));
+            }
+        } catch (CelEvaluationException e) {
+            // Report any evaluation errors, if present
+            throw new IllegalArgumentException(
+                "Evaluation error has occurred. Reason: " + e.getMessage(), e);
+        } catch (Exception e) {
+            // Log or handle evaluation errors
+            System.err.println("Error evaluating CEL expression: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void close() {
+        // Cleanup resources if necessary
+    }
+
+}
 
     // public static class CELFilter extends AbstractProcessor<String, String> {
     //     private final ProcessorConfig config;
